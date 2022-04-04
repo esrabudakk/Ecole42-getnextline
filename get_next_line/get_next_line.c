bf06d7d9d696039c0007f8c3ebfbfd2e304320f1
Esra Budak <esra_b1644@outlook.com>
2022-04-04 16:58:28 +0300

    %125

---
+++ get_next_line/get_next_line.c
@@ -0,0 +1,123 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/18 16:35:49 by ebudak            #+#    #+#             */
+/*   Updated: 2022/04/04 16:39:34 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+#include <fcntl.h>
+
+char	*return_line(char *main_line)
+{
+	char	*result_line;
+	int		i;
+
+	i = 0;
+	if (!main_line[i])
+		return (NULL);
+	while (main_line[i] != '\n' && main_line[i])
+		i++;
+	result_line = (char *)malloc(sizeof(char) * i + 2);
+	if (!result_line)
+		return (NULL);
+	i = 0;
+	while (main_line[i] != '\n' && main_line[i])
+	{
+		result_line[i] = main_line[i];
+		i++;
+	}
+	if (main_line[i] == '\n')
+	{
+		result_line[i] = '\n';
+		i++;
+	}
+	result_line[i] = '\0';
+	return (result_line);
+}
+
+char	*next_line(char *main_line)
+{
+	int		i;
+	int		c;
+	char	*satir;
+
+	i = 0;
+	while (main_line[i] != '\n' && main_line[i])
+		i++;
+	if (!main_line[i])
+	{
+		free (main_line);
+		return (NULL);
+	}
+	satir = (char *)malloc(sizeof(char) * (ft_strlen(main_line) - i + 1));
+	if (!satir)
+		return (NULL);
+	i++;
+	c = 0;
+	while (main_line[i])
+		satir[c++] = main_line[i++];
+	satir[c] = '\0';
+	free(main_line);
+	return (satir);
+}
+
+char	*read_line(int fd, char *main_line)
+{
+	int		read_value;
+	char	*part_line;
+
+	read_value = 1;
+	part_line = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1);
+	if (!part_line)
+		return (NULL);
+	while (!ft_strchr(main_line, '\n') && read_value != 0)
+	{
+		read_value = read(fd, part_line, BUFFER_SIZE);
+		if (read_value == -1)
+		{
+			free (part_line);
+			return (NULL);
+		}
+		part_line[read_value] = '\0';
+		main_line = ft_strjoin(main_line, part_line);
+	}
+	free (part_line);
+	return (main_line);
+}
+
+char	*get_next_line(int fd)
+{
+	char		*result_line;
+	static char	*main_line;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+	{
+		return (NULL);
+	}
+	main_line = read_line(fd, main_line);
+	if (!main_line)
+	{
+		return (NULL);
+	}
+	result_line = return_line(main_line);
+	main_line = next_line(main_line);
+	return (result_line);
+}
+// int main()
+// {
+// 	int	fd;
+// 	fd = open("text1.txt",O_RDONLY);
+// 	char *str;
+// 	str = get_next_line(fd);
+// 	while(str)
+// 	{		
+// 		printf("%s",str);
+// 		str = get_next_line(fd);
+// 	}
+// }
---
+++ get_next_line/get_next_line_bonus.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ebudak <ebudak@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/18 16:36:28 by ebudak            #+#    #+#             */
+/*   Updated: 2022/03/18 16:36:29 by ebudak           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 5
+# endif
+
+size_t	ft_strlen(char *str);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_strchr(char *s, int c);
+size_t	ft_strlen(char *str);
+char	*read_line(int fd, char *main_line);
+char	*return_line(char *main_line);
+char	*next_line(char *main_line);
+char	*get_next_line(int fd);
+
+#endif
